7d7cfb8b246209ffb8f2891421915246efd5f322
Shubham Lokhande <lokhande.shubham248@gmail.com>
2024-02-09 17:37:22 +0000

    Conversion learning

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+// let score = "33";
+
+// console.log(typeof score)
+
+// console.log(typeof(score))
+
+// let valInNum = Number(score);
+
+// console.log(valInNum);
+
+// console.log(typeof valInNum)
+
+
+//"33" => 33(number)
+//"33abc" => NaN(number)
+//"true" => 1 (number)
+//"false" => 0 (number)
+//"null" => 0 (number)
+
+
+let isLoggedIn2 = ""
+
+let boolIsLoggedIn = Boolean(isLoggedIn2)
+
+console.log(boolIsLoggedIn);
+console.log(typeof boolIsLoggedIn);
+
+//"" => false
+//"anyText" => true
+//1 => true
+//0 => false
+
